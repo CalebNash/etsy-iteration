a76fb833eec79130012867c47bd4d08014a25efa
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-09 15:13:31 -0400

    display items between 14 and 18 USD

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 //function average to return the average price of the items
-function itemsAverage(){
-  var prices = [];
+function itemsAverage() {
 
+var prices = [];
   //forEach method to traverse and return the price of each item
-  items.forEach(function(item, index, array){
-     prices.push(array[index].price);
+  items.forEach(function(item, index, array) {
+    prices.push(array[index].price);
   });
+  //reduce method to add the elements to eachother and equal it to total
   var total = prices.reduce(function(sum, current) {
     return sum + current;
   });
- var average = total / items.length;
-console.log(average.toFixed(2));
-return average.toFixed(2);
+  //get the average of the prices of items
+  var average = total / items.length;
+  console.log(`The average price is $${average.toFixed(2)}`);
+  return average.toFixed(2);
 }
 itemsAverage();
-//console.log(items[0].price);
+
+ function cheap() {
+
+var cheapItems = [];
+
+items.forEach(function(items, index, array){
+  price = array[index].price;
+  currencyCode = array[index].currency_code;
+  if (price<= 18 && price >= 14 && currencyCode == 'USD'){
+    cheapItems.push(array[index].title);
+  }
+});
+  console.log(`Items that cost between $14.00 USD and $18.00 USD:${cheapItems}`);
+}
+cheap();
+//console.log(prices);
