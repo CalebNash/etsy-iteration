5767ecfc5bdb6273ad4076e0678487a4932f93a9
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-10 10:00:01 -0400

    completed no comments

--- main.js
+++ main.js
@@ -43,46 +43,57 @@ cheap();
 //function globalCurrency to return items that have the GBP currency
 function globalCurrency(){
 
-  //create array for items with a GBP currency
-  var code = [];
-  //forEach method to traverse items and add items into array code
-  items.forEach(function (items, index, array){
-    currencyCode = array[index].currency_code;
-    titlePrice = array[index].title + ' is $'+ array[index].price;
-    price = array[index].price;
-    //if statement to check if currency equals GBP
-    if (currencyCode == 'GBP'){
-      code.push(titlePrice);
 
-    }
-
+  britishItems = items.filter(function(item){
+    return item.currency_code === 'GBP';
   });
-  console.log(code);
-  return code;
+
+  britishItems.forEach(function(item){
+    console.log(item.title + ' is £'+ item.price);
+  })
 }
 //call globalCurrency
 globalCurrency();
 
 //function woodenItems to return items made of wood
 function woodenItems(){
-  var woodItems = [];
 
-  //forEach method to traverse items and add items into woodItems
-  items.forEach(function(item, index, array){
-    materials = array[index].materials;
+    const woodItems = items.filter(function(item){
+      return item.materials.includes('wood');
+    });
+    const woodLog = woodItems.forEach(function(item){
+      console.log(`${item.title} is made of wood`);
+    })
 
-    //filter method to traverse the materials of each item and check if wood is present
-    var wood = materials.filter(function(it){
-      return it == 'wood';
-    });
-    //if statement to add items that contain wood to the woodItems array
-    if(wood == 'wood'){
-      woodItems.push(`${array[index].title} is made of wood`)
-    }
-    });
-    console.log(woodItems);
-    return woodItems;
+
   }
 
 //call woodenItems
 woodenItems();
+
+
+ function largeItems(){
+
+  const moreThanEight = items.filter(function(item){
+    return item.materials.length >= 8;
+  })
+  const matLog= moreThanEight.forEach(function(item){
+    console.log(`${item.title} has ${item.materials.length} materials:`);
+
+    item.materials.forEach(function(item){
+      console.log(item);
+    })
+    console.log('\n');
+  });
+}
+
+largeItems();
+
+
+function madeBySellers(){
+  const madeBySeller = items.filter(function(item){
+    return item.who_made === 'i_did';
+  });
+  console.log(`${madeBySeller.length} were made by their sellers`);
+}
+madeBySellers()
